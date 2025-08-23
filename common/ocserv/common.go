b6b751d9e6a22ad5c119417c4f46adf411d8dd6c
Masoud <mmtaee64@gmail.com>
2025-08-23 22:07:09 +0330

    add common ocserv vars

---
+++ common/ocserv/common.go
@@ -0,0 +1,9 @@
+package ocserv
+
+const (
+	OcpasswdPath       = "/etc/ocserv/ocpasswd"
+	OcpasswdExec       = "/usr/bin/ocpasswd"
+	ConfigGroupBaseDir = "/etc/ocserv/groups/"
+	DefaultGroupFile   = "/etc/ocserv/defaults/group.conf"
+	ConfigUserBaseDir  = "/etc/ocserv/users/"
+)
