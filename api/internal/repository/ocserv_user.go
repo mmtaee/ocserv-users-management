1bec23de5a0846ea48341f3987b458630f8bad2b
Masoud <mmtaee64@gmail.com>
2025-08-24 13:42:16 +0330

    api: ocserv user repository added

---
+++ api/internal/repository/ocserv_user.go
@@ -0,0 +1,255 @@
+package repository
+
+import (
+	"context"
+	"github.com/mmtaee/ocserv-users-management/api/pkg/request"
+	"github.com/mmtaee/ocserv-users-management/common/models"
+	"github.com/mmtaee/ocserv-users-management/common/ocserv/user"
+	"github.com/mmtaee/ocserv-users-management/common/pkg/database"
+	"gorm.io/gorm"
+	"time"
+)
+
+type OcservUserRepository struct {
+	db                   *gorm.DB
+	commonOcservUserRepo user.OcservUserInterface
+}
+
+type OcservUserRepositoryInterface interface {
+	//Users(ctx context.Context, pagination *request.Pagination) (*[]models.OcservUser, int64, error)
+	//Create(ctx context.Context, user *models.OcservUser) (*models.OcservUser, error)
+	//GetByUID(ctx context.Context, uid string) (*models.OcservUser, error)
+	//Update(ctx context.Context, ocservUser *models.OcservUser) (*models.OcservUser, error)
+	//Lock(ctx context.Context, uid string) error
+	//UnLock(ctx context.Context, uid string) error
+	//Delete(ctx context.Context, uid string) error
+	//TenDaysStats(ctx context.Context) (*[]models.DailyTraffic, error)
+	//UpdateUsersByDeleteGroup(ctx context.Context, groupName string) (*[]models.OcservUser, error)
+	//UserStatistics(ctx context.Context, uid string, dateStart, dateEnd *time.Time) (*[]models.DailyTraffic, error)
+	//Statistics(ctx context.Context, dateStart, dateEnd *time.Time) (*[]models.DailyTraffic, error)
+}
+
+func NewtOcservUserRepository() *OcservUserRepository {
+	return &OcservUserRepository{
+		db:                   database.GetConnection(),
+		commonOcservUserRepo: user.NewOcservUser(),
+	}
+}
+
+func (o *OcservUserRepository) Users(
+	ctx context.Context, pagination *request.Pagination,
+) (*[]models.OcservUser, int64, error) {
+	var totalRecords int64
+
+	err := o.db.WithContext(ctx).Model(&models.OcservUser{}).Count(&totalRecords).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var ocservUser []models.OcservUser
+	txPaginator := request.Paginator(ctx, o.db, pagination)
+	err = txPaginator.Model(&ocservUser).Find(&ocservUser).Error
+	if err != nil {
+		return nil, 0, err
+	}
+	return &ocservUser, totalRecords, nil
+}
+
+func (o *OcservUserRepository) Create(ctx context.Context, ocservUser *models.OcservUser) (*models.OcservUser, error) {
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(ocservUser).Error; err != nil {
+			return err
+		}
+		if err := o.commonOcservUserRepo.Create(ocservUser.Group, ocservUser.Username, ocservUser.Password, ocservUser.Config); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ocservUser, err
+}
+
+func (o *OcservUserRepository) GetByUID(ctx context.Context, uid string) (*models.OcservUser, error) {
+	var ocservUser models.OcservUser
+	err := o.db.WithContext(ctx).Where("uid = ?", uid).First(&ocservUser).Error
+	if err != nil {
+		return nil, err
+	}
+	return &ocservUser, nil
+}
+
+func (o *OcservUserRepository) Update(ctx context.Context, ocservUser *models.OcservUser) (*models.OcservUser, error) {
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Save(&ocservUser).Error; err != nil {
+			return err
+		}
+		if err := o.commonOcservUserRepo.Create(ocservUser.Group, ocservUser.Username, ocservUser.Password, ocservUser.Config); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ocservUser, nil
+}
+
+func (o *OcservUserRepository) Lock(ctx context.Context, uid string) error {
+	var ocservUser models.OcservUser
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("uid = ?", uid).First(&ocservUser).Error; err != nil {
+			return err
+		}
+		if err := tx.
+			Model(&models.OcservUser{}).
+			Where("uid = ?", uid).
+			Updates(map[string]interface{}{
+				"is_locked":      true,
+				"deactivated_at": time.Now(),
+			}).Error; err != nil {
+			return err
+		}
+
+		if _, err := o.commonOcservUserRepo.Lock(ocservUser.Username); err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func (o *OcservUserRepository) UnLock(ctx context.Context, uid string) error {
+	var ocservUser models.OcservUser
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("uid = ?", uid).First(&ocservUser).Error; err != nil {
+			return err
+		}
+		if err := tx.
+			Model(&models.OcservUser{}).
+			Where("uid = ?", uid).
+			Updates(map[string]interface{}{
+				"is_locked":      false,
+				"deactivated_at": nil,
+			}).Error; err != nil {
+			return err
+		}
+
+		if _, err := o.commonOcservUserRepo.UnLock(ocservUser.Username); err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func (o *OcservUserRepository) Delete(ctx context.Context, uid string) error {
+	var ocservUser models.OcservUser
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("uid = ?", uid).First(&ocservUser).Error; err != nil {
+			return err
+		}
+		if err := tx.Delete(&ocservUser).Error; err != nil {
+			return err
+		}
+		if _, err := o.commonOcservUserRepo.Delete(ocservUser.Username); err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func (o *OcservUserRepository) TenDaysStats(ctx context.Context) (*[]models.DailyTraffic, error) {
+	var results []models.DailyTraffic
+
+	err := o.db.WithContext(ctx).
+		Model(&models.OcservUserTrafficStatistics{}).
+		Select(`
+		DATE(created_at) AS date,
+		SUM(rx) / 1073741824.0 AS rx,
+		SUM(tx) / 1073741824.0 AS tx`).
+		Where("created_at >= ?", time.Now().AddDate(0, 0, -10)).
+		Group("DATE(created_at)").
+		Order("DATE(created_at)").
+		Scan(&results).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return &results, nil
+}
+
+func (o *OcservUserRepository) UpdateUsersByDeleteGroup(ctx context.Context, groupName string) (*[]models.OcservUser, error) {
+	var users []models.OcservUser
+
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("`group` = ?", groupName).Select("id", "group", "username").Find(&users).Error; err != nil {
+			return err
+		}
+
+		if err := tx.Model(&models.OcservUser{}).
+			Where("`group` = ?", groupName).
+			Update("group", "defaults").Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return &users, err
+}
+
+func (o *OcservUserRepository) UserStatistics(ctx context.Context, uid string, dateStart, dateEnd *time.Time) (*[]models.DailyTraffic, error) {
+	var results []models.DailyTraffic
+
+	query := o.db.WithContext(ctx).
+		Model(&models.OcservUserTrafficStatistics{}).
+		Joins("JOIN ocserv_users ou ON ou.id = ocserv_user_traffic_statistics.oc_user_id").
+		Where("ou.uid = ?", uid).
+		Select(`
+		DATE(ocserv_user_traffic_statistics.created_at) AS date,
+		SUM(ocserv_user_traffic_statistics.rx) / 1073741824.0 AS rx,
+		SUM(ocserv_user_traffic_statistics.tx) / 1073741824.0 AS tx
+	`)
+
+	if dateStart != nil {
+		query = query.Where("ocserv_user_traffic_statistics.created_at >= ?", *dateStart)
+	}
+	if dateEnd != nil {
+		query = query.Where("ocserv_user_traffic_statistics.created_at <= ?", *dateEnd)
+	}
+
+	err := query.
+		Group("DATE(ocserv_user_traffic_statistics.created_at)").
+		Order("DATE(ocserv_user_traffic_statistics.created_at)").
+		Scan(&results).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return &results, nil
+}
+
+func (o *OcservUserRepository) Statistics(ctx context.Context, dateStart, dateEnd *time.Time) (*[]models.DailyTraffic, error) {
+	var results []models.DailyTraffic
+	err := o.db.WithContext(ctx).
+		Model(&models.OcservUserTrafficStatistics{}).
+		Joins("JOIN ocserv_users ou ON ou.id = ocserv_user_traffic_statistics.oc_user_id").
+		Select(`
+		DATE(ocserv_user_traffic_statistics.created_at) AS date,
+		SUM(ocserv_user_traffic_statistics.rx) / 1073741824.0 AS rx,
+		SUM(ocserv_user_traffic_statistics.tx) / 1073741824.0 AS tx
+	`).
+		Where("ocserv_user_traffic_statistics.created_at >= ?", *dateStart).
+		Where("ocserv_user_traffic_statistics.created_at <= ?", *dateEnd).
+		Group("DATE(ocserv_user_traffic_statistics.created_at)").
+		Order("DATE(ocserv_user_traffic_statistics.created_at)").
+		Scan(&results).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return &results, nil
+}
