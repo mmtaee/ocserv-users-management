b71cdfe496c99c221bc01b4942a2ccf441441b8b
Masoud <mmtaee64@gmail.com>
2025-08-24 13:25:29 +0330

    api: add ocserv group repository

---
+++ api/internal/repository/ocserv_group.go
@@ -0,0 +1,129 @@
+package repository
+
+import (
+	"context"
+	"github.com/mmtaee/ocserv-users-management/api/pkg/request"
+	"github.com/mmtaee/ocserv-users-management/common/models"
+	"github.com/mmtaee/ocserv-users-management/common/ocserv/group"
+	"github.com/mmtaee/ocserv-users-management/common/pkg/database"
+	"gorm.io/gorm"
+	"log"
+)
+
+type OcservGroupRepository struct {
+	db              *gorm.DB
+	ocservGroupRepo group.OcservGroupInterface
+}
+
+type OcservGroupRepositoryInterface interface {
+	Groups(ctx context.Context, pagination *request.Pagination) (*[]models.OcservGroup, int64, error)
+	GroupsLookup(ctx context.Context) ([]string, error)
+	GetByID(ctx context.Context, id string) (*models.OcservGroup, error)
+	Create(ctx context.Context, ocservGroup *models.OcservGroup) (*models.OcservGroup, error)
+	Update(ctx context.Context, ocservGroup *models.OcservGroup) (*models.OcservGroup, error)
+	Delete(ctx context.Context, id string) (*models.OcservGroup, error)
+}
+
+func NewOcservGroupRepository() *OcservGroupRepository {
+	return &OcservGroupRepository{
+		db:              database.GetConnection(),
+		ocservGroupRepo: group.NewOcservGroup(),
+	}
+}
+
+func (o *OcservGroupRepository) Groups(
+	ctx context.Context, pagination *request.Pagination,
+) (*[]models.OcservGroup, int64, error) {
+	var totalRecords int64
+
+	err := o.db.WithContext(ctx).Model(&models.OcservGroup{}).Count(&totalRecords).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var ocservGroups []models.OcservGroup
+	txPaginator := request.Paginator(ctx, o.db, pagination)
+	err = txPaginator.Model(&ocservGroups).Find(&ocservGroups).Error
+	if err != nil {
+		return nil, 0, err
+	}
+	return &ocservGroups, totalRecords, nil
+}
+
+func (o *OcservGroupRepository) GroupsLookup(ctx context.Context) ([]string, error) {
+	var ocservGroups []models.OcservGroup
+
+	err := o.db.WithContext(ctx).Model(&models.OcservGroup{}).Select("name").Find(&ocservGroups).Error
+	if err != nil {
+		return nil, err
+	}
+
+	groups := make([]string, 0, len(ocservGroups))
+	for _, ocservGroup := range ocservGroups {
+		groups = append(groups, ocservGroup.Name)
+	}
+	return groups, nil
+}
+
+func (o *OcservGroupRepository) GetByID(ctx context.Context, id string) (*models.OcservGroup, error) {
+	var ocservGroup models.OcservGroup
+	err := o.db.WithContext(ctx).Where("id = ?", id).Find(&ocservGroup).Error
+	if err != nil {
+		return nil, err
+	}
+	return &ocservGroup, nil
+}
+
+func (o *OcservGroupRepository) Create(ctx context.Context, ocservGroup *models.OcservGroup) (*models.OcservGroup, error) {
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(ocservGroup).Error; err != nil {
+			return err
+		}
+		if err := o.ocservGroupRepo.Create(ocservGroup.Name, ocservGroup.Config); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return ocservGroup, nil
+}
+
+func (o *OcservGroupRepository) Update(ctx context.Context, ocservGroup *models.OcservGroup) (*models.OcservGroup, error) {
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(ocservGroup).Save(ocservGroup).Error; err != nil {
+			return err
+		}
+		if err := o.ocservGroupRepo.Create(ocservGroup.Name, ocservGroup.Config); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ocservGroup, nil
+}
+
+func (o *OcservGroupRepository) Delete(ctx context.Context, id string) (*models.OcservGroup, error) {
+	var ocservGroup models.OcservGroup
+	err := o.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("id = ?", id).First(&ocservGroup).Error; err != nil {
+			return err
+		}
+
+		if err := tx.Delete(&ocservGroup).Error; err != nil {
+			log.Println("err: ", err)
+			return err
+		}
+
+		if err := o.ocservGroupRepo.Delete(ocservGroup.Name); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return &ocservGroup, err
+}
